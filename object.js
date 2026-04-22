e56b3bc2a2b56cfc317f0bb046f35e3e79214c82
Ekta Sollet <ektasollet12@gmail.com>
2026-04-22 15:53:11 +0530

    Initial commit

---
+++ object.js
@@ -0,0 +1,16 @@
+//OBJECT 
+
+//Creating Object
+let user = new Object();
+let user = {};
+
+//Literal and Properties
+let user = {
+    name : "John",
+    age : 30
+};
+
+//Accessing property value - using dot notation
+alert(user.name);
+alert(user.age);
+
